8a411602ad1033ca53de537f576256f17ba5dcb5
dan9550 <56kflyingtoaster@gmail.com>
2013-04-18 11:45:47 +1000

    yeah... gave it a shot here, no idea :P

--- OpenRA.Mods.RA/SupportPowers/SonarPulsePower.cs
+++ OpenRA.Mods.RA/SupportPowers/SonarPulsePower.cs
@@ -8,24 +8,47 @@
  */
 #endregion
 
+using System.Linq;
+using System.Drawing;
+using System.Collections.Generic;
+using OpenRA.Effects;
+using OpenRA.Mods.RA.Effects;
 using OpenRA.Traits;
 
 namespace OpenRA.Mods.RA
 {
-	public class SonarPulsePowerInfo : SupportPowerInfo
+	public class SonarPulsePowerInfo : SupportPowerInfo , ITraitInfo
 	{
-		public override object Create(ActorInitializer init) { return new SonarPulsePower(init.self, this); }
+		public override object Create(ActorInitializer init) { return new PulseDetect(init.self.Owner); }
 	}
 
+    class PulseDetect : ISync
+    {
+        Player owner;
+        List<Actor> actors = new List<Actor> { };
+
+        public PulseDetect( Player owner ) { this.owner = owner; }
+    }
 	public class SonarPulsePower : SupportPower
 	{
+
+        List<Actor> actors = new List<Actor> { };
+
 		public SonarPulsePower(Actor self, SonarPulsePowerInfo info) : base(self, info) { }
-		public override void Activate(Actor self, Order order)
+
+		public override void Activate(Actor spen, Order order)
 		{
 			// TODO: Reveal submarines
 
 			// Should this play for all players?
-			Sound.Play("sonpulse.aud");
+			//Sound.Play("sonpulse.aud");
+            self.World.AddFrameEndTask(w =>
+            {
+                Sound.PlayToPlayer(self.Owner, Info.LaunchSound);
+
+                foreach (TraitPair<PulseDetect> i in spen.World.ActorsWithTrait<PulseDetect>())
+
+            });
 		}
 	}
 }
